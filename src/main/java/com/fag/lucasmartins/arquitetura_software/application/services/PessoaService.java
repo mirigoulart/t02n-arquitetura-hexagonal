157edd913edcc9e7fc5e1ced9cc11d7c2b080707
mirigoulart <mirigoulartdossantos@gmail.com>
2026-04-06 22:00:32 -0300

    Arquitetura Hexagonal

---
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/application/services/PessoaService.java
@@ -0,0 +1,26 @@
+package com.fag.lucasmartins.arquitetura_software.application.services;
+
+import com.fag.lucasmartins.arquitetura_software.application.ports.in.service.PessoaServicePort;
+import com.fag.lucasmartins.arquitetura_software.application.ports.out.persistence.h2.PessoaRepositoryPort;
+import com.fag.lucasmartins.arquitetura_software.core.domain.bo.PessoaBO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService implements PessoaServicePort {
+
+    private final PessoaRepositoryPort pessoaRepositoryPort;
+
+    public PessoaService(PessoaRepositoryPort pessoaRepositoryPort) {
+        this.pessoaRepositoryPort = pessoaRepositoryPort;
+    }
+
+    @Override
+    public PessoaBO salvar(PessoaBO pessoaBO) {
+        pessoaBO.validarMaioridade();
+        pessoaBO.validarCpf();
+        pessoaBO.validarEmail();
+        pessoaBO.validarTelefone();
+
+        return pessoaRepositoryPort.salvar(pessoaBO);
+    }
+}
