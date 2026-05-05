6d3ce502a4503a07836e13ec68b2f5f284663859
mirigoulart <mirigoulartdossantos@gmail.com>
2026-05-05 20:14:14 -0300

    Event-Driven Architecture - Atualizações

--- src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/pedido/listener/SqsPedidoAdapter.java
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/pedido/listener/SqsPedidoAdapter.java
@@ -2,13 +2,18 @@ package com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.mes
 
 import com.fag.lucasmartins.arquitetura_software.application.ports.in.service.PedidoServicePort;
 import com.fag.lucasmartins.arquitetura_software.core.domain.bo.PedidoBO;
+import com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.messaging.exceptions.ConsumerSQSException;
 import com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.messaging.pedido.dto.EventoPedidoSqsDTO;
 import com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.messaging.pedido.mapper.EventoPedidoSqsMapper;
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SqsPedidoAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(SqsPedidoAdapter.class);
 
     private final PedidoServicePort pedidoServicePort;
     private final EventoPedidoSqsMapper mapper;
@@ -20,15 +25,16 @@ public class SqsPedidoAdapter {
 
     @SqsListener("${queue.order-events}")
     public void listen(EventoPedidoSqsDTO dto) {
-        System.out.println("Mensagem recebida com sucesso para o cliente ID: " + dto.getCustomerId());
+        try {
+            log.info("Evento de pedido recebido para o cliente {}", dto.getCustomerId());
 
-        PedidoBO criado = process(dto);
+            final PedidoBO pedidoBO = mapper.toBO(dto);
+            final PedidoBO criado = pedidoServicePort.criarPedido(pedidoBO);
 
-        System.out.println("Pedido criado com sucesso no sistema. ID do pedido: " + criado.getId());
-    }
-
-    private PedidoBO process(EventoPedidoSqsDTO dto) {
-        PedidoBO pedidoBO = mapper.toBO(dto);
-        return pedidoServicePort.criarPedido(pedidoBO);
+            log.info("Pedido criado com sucesso no sistema. ID do pedido: {}", criado.getId());
+        } catch (Exception e) {
+            log.error("Erro ao processar o evento de pedido para o cliente {}", dto.getCustomerId(), e);
+            throw new ConsumerSQSException("Erro ao processar o evento de pedido para o cliente " + dto.getCustomerId(), e);
+        }
     }
 }
